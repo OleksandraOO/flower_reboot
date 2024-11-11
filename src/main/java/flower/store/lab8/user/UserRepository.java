fadaa703eaae0d72576dd77abb295ffdbdeaea2c
Oleksandra <ostafiichuk.pn@ucu.edu.ua>
2024-11-11 22:56:21 +0200

    added user

---
+++ src/main/java/flower/store/lab8/user/UserRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+ package flower.store.lab8.user;
+
+ import java.util.Optional;
+ 
+ import org.springframework.data.jpa.repository.Query;
+ import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+ 
+ 
+ public interface  UserRepository extends  JpaRepositoryImplementation<AppUser, Long> {
+ 
+     @Query("SELECT u FROM AppUser u WHERE u.email = ?1")
+     Optional<AppUser> findUserByEmail(String email);
+ 
+ }
+ 
